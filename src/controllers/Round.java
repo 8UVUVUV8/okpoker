3b1d48f076409bb3cef87bb3ea2774fcd5641aac
szekeres miklos <hiimmikescott@gmail.com>
2023-11-29 10:13:30 +0100

    asd

---
+++ src/controllers/Round.java
@@ -0,0 +1,20 @@
+/*
+* File: Round.java
+* Author: Nagy József
+* Copyright: 2021, Nagy József 
+* Date: 2021-09-11
+* Licenc: MIT
+* refactored by: Szekeres András
+*
+*/
+
+package controllers;
+
+public enum Round {
+        PREFLOP,
+        FLOP,
+        TURN,
+        RIVER,
+        SHOW
+    
+}
--- src/views/MainWindow.java
+++ src/views/MainWindow.java
@@ -4,18 +4,20 @@
 * Copyright: 2021, Nagy József 
 * Date: 2021-09-11
 * Licenc: MIT
+* refactored by: Szekeres András
 *
 */
+
 package views;
 
 import java.awt.Color;
-
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainWindow extends JFrame {
+
     public JButton startBtn = new JButton("Start");
     public JButton stopBtn = new JButton("Stop");
     public JButton nextBtn = new JButton("Következő");
@@ -30,20 +32,17 @@ public class MainWindow extends JFrame {
     public JButton turnButton = new JButton();
     public JButton riverButton = new JButton();
 
-
     public MainWindow() {
         this.tablePanel.setSize(100, 100);
         this.tablePanel.setBackground(Color.LIGHT_GRAY);
         this.tablePanel.add(flop1Btn);
         this.tablePanel.add(flop2Btn);
         this.tablePanel.add(flop3Btn);
-        /* A flop-nak nem kell megjelennie flop előtti állapotban */
         this.flop1Btn.setVisible(false);
         this.flop2Btn.setVisible(false);
         this.flop3Btn.setVisible(false);
         this.handPanel.add(this.humanCard1Btn);
         this.handPanel.add(this.humanCard2Btn);
-
         this.buttonPanel.add(startBtn);
         this.buttonPanel.add(nextBtn);
         this.buttonPanel.add(stopBtn);
@@ -51,8 +50,6 @@ public class MainWindow extends JFrame {
         this.tablePanel.add(riverButton);
         this.turnButton.setVisible(false);
         this.riverButton.setVisible(false);
-                
-
         this.setLayout(new BoxLayout(
             this.getContentPane(), 
             BoxLayout.PAGE_AXIS));
